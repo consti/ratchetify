a4ea4f3255cf1ad1f4db45e40244d083eb6d6dfd
Michael Kuehl <mickuehl@googlemail.com>
2014-03-20 09:52:53 +0100

    refactoring

--- lib/ratchetify/test.rb
+++ lib/ratchetify/test.rb
@@ -2,7 +2,7 @@
 Capistrano::Configuration.instance.load do
   
   desc "Tasks to test the login credentials"
-  namespace :test do
+  namespace :login_test do
     desc "Test the login credentials"
     task :default do
       run "pwd && whoami"

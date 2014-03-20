d5b44c21219c7d83043fbe5bbc4e6f91443a035f
Michael Kuehl <mickuehl@googlemail.com>
2014-03-20 10:00:59 +0100

    refactoring

--- lib/ratchetify.rb
+++ lib/ratchetify.rb
@@ -4,6 +4,11 @@ Capistrano::Configuration.instance.load do
   
   desc "Prepare a new uberspace for deployment"
   namespace :prepare do
+    
+    desc "Tasks to prepare an uberspace for deployment"
+    task :default do
+    end
+    
   end
 end
 

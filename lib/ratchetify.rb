d29d710dbd0839f885c6f7ff323dd49f9c8072b3
Michael Kuehl <mickuehl@googlemail.com>
2014-03-21 09:47:28 +0100

    refactoring

--- lib/ratchetify.rb
+++ lib/ratchetify.rb
@@ -4,6 +4,7 @@ Capistrano::Configuration.instance.load do
   
   require 'capistrano'
   require 'ratchetify/helpers'
+  require 'ratchetify/setup'
   require 'ratchetify/ruby'
   
   # host and domain for the new app
@@ -19,39 +20,4 @@ Capistrano::Configuration.instance.load do
   set :conf_dir, "#{home}/aconf"
   set :ruby_version, "2.0.0"
   
-  desc "Prepare a new uberspace for deployment"
-  namespace :setup do
-    
-    desc "Tasks to prepare an uberspace for deployment"
-    task :environment do
-      
-      unless file_exists? '.ratchet'
-      
-        puts "Initializing uberspace '#{user}'"
-        
-        # save the current .bashrc
-        run "cp .bashrc bashrc.bak"
-        
-        # create the necessary folders
-        create_dir app_dir unless dir_exists? app_dir
-        create_dir conf_dir unless dir_exists? conf_dir
-          
-        # enable ruby
-        ruby
-        # activate the daemontools
-        run "uberspace-setup-svscan"
-        
-        # done
-        run "touch .ratchet"
-      end  
-    end # task :environment
-    
-    desc "removes all changes"
-    task :clean do
-      run "mv bashrc.bak .bashrc"
-      run "rm -rf .gem .gemrc .ratchet"
-      # keep ~/apps and ~/aconf for now ...
-    end # task :clean
-    
-  end
 end

9373fb2b4f6cd6dd9cfbbefb72625212712635b7
Michael Kuehl <mickuehl@googlemail.com>
2014-03-21 09:14:10 +0100

    refactoring

--- lib/ratchetify.rb
+++ lib/ratchetify.rb
@@ -6,30 +6,40 @@ Capistrano::Configuration.instance.load do
   require 'ratchetify/helpers'
   require 'ratchetify/ruby'
   
-  set :home, "~"
+  # host and domain for the new app
   set :domain, nil
   set :host, nil
   
-  set :daemon_port, rand(61000-32768+1)+32768  # random ephemeral port
+  # random port used for the reverse proxy
+  set :daemon_port, rand(61000-32768+1)+32768  # random port
+  
+  # internal variables, should not be changed in the cap file
+  set :home, "~"
   set :app_dir, "#{home}/apps"
   set :conf_dir, "#{home}/aconf"
+  set :ruby_version, "2.0.0"
   
   desc "Prepare a new uberspace for deployment"
   namespace :prepare do
     
     desc "Tasks to prepare an uberspace for deployment"
     task :default do
-      if not file_exists? '.ratchet'
-
+      
+      do unless file_exists? '.ratchet'
+      
         puts "Initializing uberspace '#{user}'"
         
-        create_dir(app_dir) unless dir_exists? (app_dir)
+        # create the necessary folders
+        create_dir app_dir unless dir_exists? app_dir
         create_dir conf_dir unless dir_exists? conf_dir
           
-        #run "touch .ratchet"
-      else
-        puts "Uberspace has already been initialized."
+        # enable ruby
+        setup:ruby
+        
+        # done
+        run "touch .ratchet"
       end
+      
     end
     
     desc "Test the login credentials"

2b500bdce8b624425dc407b1ebdbf4276c7edf48
Michael Kuehl <mickuehl@googlemail.com>
2014-03-24 17:28:03 +0100

    wordpress tasks, refactored RAILS tasks

--- lib/ratchetify/wordpress.rb
+++ lib/ratchetify/wordpress.rb
@@ -28,11 +28,13 @@ Capistrano::Configuration.instance.load do
         run "cd #{deploy_root} && mkdir wp_tmp"
         run "cd #{deploy_root} && tar xfz wordpress*"
         run "cd #{deploy_root}/wp_tmp && mv wordpress .."
-        run "cd #{deploy_root} && mv ./wordpress #{application}"
         
         # cleanup
         run "cd #{deploy_root} && rm wordpress*"
         run "cd #{deploy_root} && rm -rf wp_tmp"
+        
+        # rename word press to -> :application
+        run "cd #{deploy_root} && mv ./wordpress #{application}"
       end
       
     end # task :create_dir

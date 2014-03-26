e4f2297638ff23400075b88e544b65a4755e21ef
Michael Kuehl <mickuehl@googlemail.com>
2014-03-26 08:36:27 +0100

    added an update task

--- update.rb
+++ update.rb
@@ -13,7 +13,33 @@ Capistrano::Configuration.instance.load do
     
     desc "Stop the service, update the app then restart"
     task :default do
-      puts "updating"
+      # switch to the specified branch
+      run "cd #{deploy_dir} && git checkout -b #{fetch :branch}" unless on_branch? deploy_dir, "#{fetch :branch}"
+      
+      # update the repo
+      git_cmd = "cd #{deploy_root} && git pull origin #{fetch :branch}"
+      
+      run git_cmd do |channel, stream, out|
+        if out =~ /Password:/
+          channel.send_data("#{fetch :repo_password}\n")
+        else
+          puts out
+        end
+      end
+      
+      #
+      # typical RAILS stuff to finalize
+      #
+      
+      # run bundle install first
+      run "cd #{deploy_dir} && bundle install --path ~/.gem"
+      
+      # run rake db:migrate to create all tables etc
+      run "cd #{deploy_dir} && bundle exec rake db:migrate RAILS_ENV=#{fetch :environment}"
+      
+      # compile assets from the asset pipeline
+      run "cd #{deploy_dir} && bundle exec rake assets:precompile RAILS_ENV=#{fetch :environment}"
+      
     end # end task :default
     
   end

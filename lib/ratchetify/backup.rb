0d95eb049cc89d931aa6ae9605bd633a070ba11c
Constantin Hofstetter <Consti@Consti.de>
2014-07-26 23:50:09 +0700

    Update backup.rb

--- lib/ratchetify/backup.rb
+++ lib/ratchetify/backup.rb
@@ -18,6 +18,7 @@ Capistrano::Configuration.instance.load do
         # database name
         postgres_database = "#{user}_#{application}"
         filename = "~/#{ postgres_database }-#{ Time.now.to_i }.sql.gz"
+        transfer :down, filename, filename, :via => :scp
 
         run "cd #{deploy_dir} && pg_dump #{ postgres_database } | gzip > #{ filename }"
         puts "Backup of #{ postgres_database } created: #{ filename }"
